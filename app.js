8886450527c25e11ab22ae4bf17593ac65b0797b
azizbek <92381227+Shakhzod2008g@users.noreply.github.com>
2022-02-22 18:46:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+alert("Извиняюсь что не вовремя!")
+let elemnts = document.getElementsByClassName("parent__baby");
+
+elemnts[0].style.backgroundColor = "red";
+elemnts[2].style.backgroundColor = "red";
+elemnts[4].style.backgroundColor = "red";
+elemnts[6].style.backgroundColor = "red";
+
+elemnts[1].style.backgroundColor = "aqua";
+elemnts[3].style.backgroundColor = "aqua";
+elemnts[5].style.backgroundColor = "aqua";
+elemnts[7].style.backgroundColor = "aqua";
